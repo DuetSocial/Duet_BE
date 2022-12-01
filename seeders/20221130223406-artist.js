db834881d3821063645c048c5f593152138d3583
Jack Viana <jack.viana2019@gmail.com>
2022-12-01 11:18:09 -0500

    seeding code

--- seeders/20221130223406-artist.js
+++ seeders/20221130223406-artist.js
@@ -3,23 +3,28 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up (queryInterface, Sequelize) {
-    /**
-     * Add seed commands here.
-     *
-     * Example:
-     * await queryInterface.bulkInsert('People', [{
-     *   name: 'John Doe',
-     *   isBetaMember: false
-     * }], {});
-    */
-  },
+    await queryInterface.bulkInsert(
+      "artists",
+      [
+        {
+          userId: 1,
+          name: "Drake",
+          artistId: 1
+        },
+        {
+          userId: 2,
+          name: "Billie Eilish",
+          artistId: 2
+        },
+        {
+          userId: 3,
+          name: "The Weeknd",
+          artistId: 3
+        }
+      ]
+  )},
 
   async down (queryInterface, Sequelize) {
-    /**
-     * Add commands to revert seed here.
-     *
-     * Example:
-     * await queryInterface.bulkDelete('People', null, {});
-     */
+    await queryInterface.bulkDelete("artists")
   }
 };
--- seeders/20221130223433-comment.js
+++ seeders/20221130223433-comment.js
@@ -3,23 +3,34 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up (queryInterface, Sequelize) {
-    /**
-     * Add seed commands here.
-     *
-     * Example:
-     * await queryInterface.bulkInsert('People', [{
-     *   name: 'John Doe',
-     *   isBetaMember: false
-     * }], {});
-    */
-  },
+    await queryInterface.bulkInsert(
+      "comments",
+      [
+        {
+          userId: 1,
+          name: "Justin Harris",
+          photo: "img.png",
+          content: "Yo this band is awesome! any other recs?",
+          commentId: 1
+        },
+        {
+          userId: 2,
+          name: "Matt Sweeney",
+          photo: "img.png",
+          content: "Billies new album is my fav of the year",
+          commentId: 2
+        },
+        {
+          userId: 3,
+          name: "Jack Viana",
+          photo: "img.png",
+          content: "The Weeknd sure can sing",
+          commentId: 3
+        }
+      ]
+  )},
 
   async down (queryInterface, Sequelize) {
-    /**
-     * Add commands to revert seed here.
-     *
-     * Example:
-     * await queryInterface.bulkDelete('People', null, {});
-     */
+    await queryInterface.bulkDelete("comments")
   }
 };
