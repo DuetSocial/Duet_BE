c20c1ef725f8c0c2320be0c2d4d8076962a44d66
Matt <sweeney.matt93@gmail.com>
2022-12-02 15:25:33 -0600

    pushing changes made while debugging with tiffany

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -26,9 +26,9 @@ const Login = async (req, res) => {
 
 const Register = async (req, res) => {
   try {
-    const { email, password, name } = req.body
+    const { userName, password } = req.body
     let passwordDigest = await middleware.hashPassword(password)
-    const user = await User.create({ email, passwordDigest, name })
+    const user = await User.create({ userName, passwordDigest })
     res.send(user)
   } catch (error) {
     throw error
