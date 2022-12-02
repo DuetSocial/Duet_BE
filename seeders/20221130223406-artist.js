167586e1a23faf8b916fa8e372d25ded8e99e48b
Justin Harris <justin.r.harris@gmail.com>
2022-12-02 11:08:26 -0500

    added genre seeders

--- seeders/20221130223406-artist.js
+++ seeders/20221130223406-artist.js
@@ -1,39 +1,37 @@
-'use strict';
+"use strict"
 
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
-  async up (queryInterface, Sequelize) {
-    await queryInterface.bulkInsert(
-      "artists",
-      [
-        {
-          user_id: 1,
-          name: "Drake",
-          artistId: 1,
-          liked: false,
-          createdAt: new Date(),
-          updatedAt: new Date()
-        },
-        {
-          user_id: 2,
-          name: "Billie Eilish",
-          artistId: 2,
-          liked: false,
-          createdAt: new Date(),
-          updatedAt: new Date()
-        },
-        {
-          user_id: 3,
-          name: "The Weeknd",
-          artistId: 3,
-          liked: true,
-          createdAt: new Date(),
-          updatedAt: new Date()
-        }
-      ]
-  )},
+  async up(queryInterface, Sequelize) {
+    await queryInterface.bulkInsert("artists", [
+      {
+        user_id: 1,
+        name: "Drake",
+        artistId: 1,
+        liked: false,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        user_id: 2,
+        name: "Billie Eilish",
+        artistId: 2,
+        liked: false,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        user_id: 3,
+        name: "The Weeknd",
+        artistId: 3,
+        liked: true,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+    ])
+  },
 
-  async down (queryInterface, Sequelize) {
+  async down(queryInterface, Sequelize) {
     await queryInterface.bulkDelete("artists")
-  }
-};
+  },
+}
