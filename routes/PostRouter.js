729a1edc741a688a00ef42d5a03bb742eaa5b1aa
Justin Harris <justin.r.harris@gmail.com>
2022-12-02 10:36:34 -0500

    backend routers created

---
+++ routes/PostRouter.js
@@ -0,0 +1,21 @@
+const router = require("express").Router()
+const controller = require("../controllers/PostController")
+const middleware = require("../middleware")
+
+router.get("/", controller.GetPosts)
+router.post(
+  "/",
+  middleware.stripToken,
+  middleware.verifyToken,
+  controller.CreatePost
+)
+router.put(
+  "/:post_id",
+  middleware.stripToken,
+  middleware.verifyToken,
+  controller.UpdatePost
+)
+
+router.delete("/:post_id", controller.DeletePost)
+
+module.exports = router
