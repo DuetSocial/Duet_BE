e2ea5c15a400e521416f5a21590f01a3bf7cda73
Jack Viana <jack.viana2019@gmail.com>
2022-12-05 12:12:02 -0500

    changes w jeremy BE

--- routes/CommentRouter.js
+++ routes/CommentRouter.js
@@ -2,7 +2,7 @@ const Router = require('express').Router()
 const controller = require('../controllers/CommentController')
 
 Router.get('/view', controller.getAllComments)
-Router.post('/:user_id', controller.addComment)
+Router.post('/', controller.addComment)
 Router.put('/:comment_id', controller.updateComment)
 Router.delete('/:comment_id', controller.deleteComment)
 
